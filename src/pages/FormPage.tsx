a579fde9a809ae27b0540478c6b64fc7674d8565
hirokiazuma0420 <hiroki.a.biz@gmail.com>
2025-07-15 17:03:12 +0900

    Revert フロートボックス固定化

--- src/pages/FormPage.tsx
+++ src/pages/FormPage.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useMemo, useEffect, useRef } from 'react';
+import React, { useState, useMemo, useEffect} from 'react';
 import { Trash2 } from "lucide-react";
 import incomeBrackets from "../assets/net_income_brackets.json";
 import AssetAccordion from "../components/AssetAccordion";
